feae380abe88ba8e86517a8ed9f1939bfc1c21e7
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-15 10:33:31 -0400

    Works! :+1:

--- src/CS/Main.php
+++ src/CS/Main.php
@@ -1,17 +1,18 @@
 <?php
 
-namspace CS;
-
+namespace CS;
+use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
     
     
     
     public function onEnable(){
-    this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
         //when you are to lazy to add a logger
     }
     
@@ -19,21 +20,25 @@ class Main extends PluginBase implements Listener{
     $p = $ev->getPlayer();
     $badwords = array("fuck","pussy","bitch","nigga","cunt","whore","asshole","bitch nigga","dick",/*spanish word for penis*/"pene",
     "penis","cock");
+    $msg = explode(" ",$ev->getMessage());
+    foreach($msg as $word){
     foreach($badwords as $m){
-    if(explode(" ",strtolower($ev->getMessage() === $m))){
+    if(strtolower($word === $m)){
+    
     $ev->setCancelled();
     $p->sendMessage("You can't say that youngster!");
     
     }
     }
-    if(explode(" ",strtolower($ev->getMessage() === "how do i see my coins?"||"how do i see my money?"))){
+    }
+    /*if(explode(" ",strtolower($ev->getMessage() === "how do i see my coins?"||"how do i see my money?"))){
     $p->sendMessage("Use /mymoney, /coins, or /bal. To see your money!");
     //canel event or nah?
-    }
-    f(explode(" ",strtolower($ev->getMessage() === "where do i go to buy a rank?"||"how to get VIP?"||"how to get vip?"))){
+    }*/
+   /* if(explode(" ",strtolower($ev->getMessage() === "where do i go to buy a rank?"||"how to get VIP?"||"how to get vip?"))){
     $p->sendMessage("go to our website!");
     //canel event or nah?
-    }
+    }*/
     }
     }
     //anti-spam soon!
